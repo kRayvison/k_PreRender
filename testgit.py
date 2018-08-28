86ed9ef251fd3ce70c2e7b7fab70916efaca27cc
kryfo <superdkk@gmail.com>
2018-08-28 19:41:13 +0800

    确认同步一下

--- testgit.py
+++ testgit.py
@@ -2,4 +2,15 @@
 #hello git
 print ('good jobs')
 
-print ('hello rayvision')
+a= 'ddddewf'
+b='ooooo'
+#print ('{q} hello rayvision {w}'.format(q=a,w=b))
+import  os
+c=os.path.realpath(__file__)
+print (c)
+
+custom_app='aaaaa'
+import  time
+print (time.time())
+import os
+os.system('cmdkey /add: 192.168.2.10 /user:abc /pass:password')
